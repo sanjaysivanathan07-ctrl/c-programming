8f6df59a7e063dedee2eaa36d6a0439bfb13a81b
sanjaysivanathan07-ctrl <sanjaysivanathan07@gmail.com>
2025-08-29 21:46:56 -0700

    Add files via upload

---
+++ cse-d,sanjay.s,programming in c/input ,output/Q-10.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int d;
+	scanf("%o",&d);
+	printf("%d",d);
+	return 0;
+}
---
+++ cse-d,sanjay.s,programming in c/input ,output/Q9.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int d;
+	scanf("%x",&d);
+	printf("%d",d);
+	return 0;
+}
